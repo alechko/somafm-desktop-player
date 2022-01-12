76b3752b3fc8a48214e42be5361bacc776751984
alechko <mail@alechko.net>
2022-01-12 10:32:39 +0200

    fixing bug.

--- src/components/Greetings/index.tsx
+++ src/components/Greetings/index.tsx
@@ -3,20 +3,16 @@ import { Container, Image, Text } from './styles'
 
 export function Greetings() {
   function handleSayHello() {
-    window.Main.sendMessage('Hello World');
+    window.Main.sendMessage('Hello World', null)
 
     console.log('Message sent! Check main process log in terminal.')
   }
 
   return (
     <Container>
-      <Image
-        src="https://www.vectorlogo.zone/logos/reactjs/reactjs-icon.svg"
-        alt="ReactJS logo"
-      />
+      <Image src="https://www.vectorlogo.zone/logos/reactjs/reactjs-icon.svg" alt="ReactJS logo" />
       <Text>An Electron boilerplate including TypeScript, React, Jest and ESLint.</Text>
       <Button onClick={handleSayHello}>Send message to main process</Button>
     </Container>
   )
 }
- 
